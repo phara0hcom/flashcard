61f72dd6d56ef452587386163e20311915b90a67
Tamer <t.elsay3d@gmail.com>
2018-02-12 21:24:55 +0100

    Added hirigana.js

---
+++ src/js/decks/hiragana.js
@@ -0,0 +1,387 @@
+const basicHiragana = [
+  //∅
+  {
+    index: "Hira_∅_a",
+    symbol: "あ",
+    roman: "a"
+  },
+  {
+    index: "Hira_∅_i",
+    symbol: "い",
+    roman: "i"
+  },
+  {
+    index: "Hira_∅_u",
+    symbol: "う",
+    roman: "u"
+  },
+  {
+    index: "Hira_∅_e",
+    symbol: "え",
+    roman: "e"
+  },
+  {
+    index: "Hira_∅_o",
+    symbol: "お",
+    roman: "o"
+  },
+  //K
+  {
+    index: "Hira_K_a",
+    symbol: "か",
+    roman: "ka"
+  },
+  {
+    index: "Hira_K_i",
+    symbol: "き",
+    roman: "ki"
+  },
+  {
+    index: "Hira_K_u",
+    symbol: "く",
+    roman: "ku"
+  },
+  {
+    index: "Hira_K_e",
+    symbol: "け",
+    roman: "ke"
+  },
+  {
+    index: "Hira_K_o",
+    symbol: "こ",
+    roman: "ko"
+  },
+  //S
+  {
+    index: "Hira_S_a",
+    symbol: "さ",
+    roman: "sa"
+  },
+  {
+    index: "Hira_S_i",
+    symbol: "し",
+    roman: "shi"
+  },
+  {
+    index: "Hira_S_u",
+    symbol: "す",
+    roman: "su"
+  },
+  {
+    index: "Hira_S_e",
+    symbol: "せ",
+    roman: "se"
+  },
+  {
+    index: "Hira_S_o",
+    symbol: "そ",
+    roman: "so"
+  },
+  //T
+  {
+    index: "Hira_T_a",
+    symbol: "た",
+    roman: "ta"
+  },
+  {
+    index: "Hira_T_i",
+    symbol: "ち",
+    roman: "chi"
+  },
+  {
+    index: "Hira_T_u",
+    symbol: "つ",
+    roman: "tsu"
+  },
+  {
+    index: "Hira_T_e",
+    symbol: "て",
+    roman: "te"
+  },
+  {
+    index: "Hira_T_o",
+    symbol: "と",
+    roman: "to"
+  },
+  //N
+  {
+    index: "Hira_N_a",
+    symbol: "な",
+    roman: "na"
+  },
+  {
+    index: "Hira_N_i",
+    symbol: "に",
+    roman: "ni"
+  },
+  {
+    index: "Hira_N_u",
+    symbol: "ぬ",
+    roman: "nu"
+  },
+  {
+    index: "Hira_N_e",
+    symbol: "ね",
+    roman: "ne"
+  },
+  {
+    index: "Hira_N_o",
+    symbol: "の",
+    roman: "no"
+  },
+  //H
+  {
+    index: "Hira_H_a",
+    symbol: "は",
+    roman: "ha[ɰa]"
+  },
+  {
+    index: "Hira_H_i",
+    symbol: "ひ",
+    roman: "hi"
+  },
+  {
+    index: "Hira_H_u",
+    symbol: "ふ",
+    roman: "fu"
+  },
+  {
+    index: "Hira_H_e",
+    symbol: "へ",
+    roman: "he[e]"
+  },
+  {
+    index: "Hira_H_o",
+    symbol: "ほ",
+    roman: "ho"
+  },
+  //M
+  {
+    index: "Hira_M_a",
+    symbol: "ま",
+    roman: "ma"
+  },
+  {
+    index: "Hira_M_i",
+    symbol: "み",
+    roman: "mi"
+  },
+  {
+    index: "Hira_M_u",
+    symbol: "む",
+    roman: "mu"
+  },
+  {
+    index: "Hira_M_e",
+    symbol: "め",
+    roman: "me"
+  },
+  {
+    index: "Hira_M_o",
+    symbol: "も",
+    roman: "mo"
+  },
+  //Y
+  {
+    index: "Hira_Y_a",
+    symbol: "や",
+    roman: "ya"
+  },
+  {
+    index: "Hira_Y_u",
+    symbol: "ゆ",
+    roman: "yu"
+  },
+  {
+    index: "Hira_Y_o",
+    symbol: "よ",
+    roman: "yo"
+  },
+  //R
+  {
+    index: "Hira_R_a",
+    symbol: "ら",
+    roman: "ra"
+  },
+  {
+    index: "Hira_R_i",
+    symbol: "り",
+    roman: "ri"
+  },
+  {
+    index: "Hira_R_u",
+    symbol: "る",
+    roman: "ru"
+  },
+  {
+    index: "Hira_R_e",
+    symbol: "れ",
+    roman: "re"
+  },
+  {
+    index: "Hira_R_o",
+    symbol: "ろ",
+    roman: "ro"
+  },
+  //W
+  {
+    index: "Hira_W_a",
+    symbol: "わ",
+    roman: "wa"
+  },
+  {
+    index: "Hira_W_i",
+    symbol: "ゐ",
+    roman: "wi"
+  },
+  {
+    index: "Hira_W_o",
+    symbol: "を",
+    roman: "wo"
+  },
+  {
+    index: "Hira_N",
+    symbol: "ん",
+    roman: "n"
+  }
+];
+
+const dakutenHiragana = [
+  //G
+  {
+    index: "Hira_G_a",
+    symbol: "が",
+    roman: "ga"
+  },
+  {
+    index: "Hira_G_i",
+    symbol: "ぎ",
+    roman: "gi"
+  },
+  {
+    index: "Hira_G_u",
+    symbol: "ぐ",
+    roman: "gu"
+  },
+  {
+    index: "Hira_G_e",
+    symbol: "げ",
+    roman: "ge"
+  },
+  {
+    index: "Hira_G_o",
+    symbol: "ご",
+    roman: "go"
+  },
+  //Z
+  {
+    index: "Hira_Z_a",
+    symbol: "ざ",
+    roman: "za"
+  },
+  {
+    index: "Hira_Z_i",
+    symbol: "じ",
+    roman: "zi"
+  },
+  {
+    index: "Hira_Z_u",
+    symbol: "ず",
+    roman: "zu"
+  },
+  {
+    index: "Hira_Z_e",
+    symbol: "ぜ",
+    roman: "ze"
+  },
+  {
+    index: "Hira_Z_o",
+    symbol: "ぞ",
+    roman: "zo"
+  },
+  //D
+  {
+    index: "Hira_D_a",
+    symbol: "だ",
+    roman: "da"
+  },
+  {
+    index: "Hira_D_i",
+    symbol: "ぢ",
+    roman: "di"
+  },
+  {
+    index: "Hira_D_u",
+    symbol: "づ",
+    roman: "dzu"
+  },
+  {
+    index: "Hira_D_e",
+    symbol: "で",
+    roman: "de"
+  },
+  {
+    index: "Hira_D_o",
+    symbol: "ど",
+    roman: "do"
+  },
+  //B
+  {
+    index: "Hira_B_a",
+    symbol: "ば",
+    roman: "ba"
+  },
+  {
+    index: "Hira_B_i",
+    symbol: "び",
+    roman: "bi"
+  },
+  {
+    index: "Hira_B_u",
+    symbol: "ぶ",
+    roman: "bu"
+  },
+  {
+    index: "Hira_B_e",
+    symbol: "べ",
+    roman: "be"
+  },
+  {
+    index: "Hira_B_o",
+    symbol: "ぼ",
+    roman: "bo"
+  },
+  //P
+  {
+    index: "Hira_P_a",
+    symbol: "ぱ",
+    roman: "pa"
+  },
+  {
+    index: "Hira_P_i",
+    symbol: "ぴ",
+    roman: "pi"
+  },
+  {
+    index: "Hira_P_u",
+    symbol: "ぷ",
+    roman: "pu"
+  },
+  {
+    index: "Hira_P_e",
+    symbol: "ぺ",
+    roman: "pe"
+  },
+  {
+    index: "Hira_P_o",
+    symbol: "ぽ",
+    roman: "po"
+  }
+];
+
+
+
+
+
+
+
