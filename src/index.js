01a354f9f0949e466850d94fba98dcddaba2f09e
Tamer <t.elsay3d@gmail.com>
2018-03-17 14:44:03 +0100

    service workers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { render } from "react-dom";
 import React from "react";
 import App from "./js/App";
+import registerServiceWorker from "./registerServiceWorker";
 
 render(<App />, document.getElementById("root"));
